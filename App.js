d92a7d393d7dc53b14318ae851ac5e71677f67eb
Jefferson <jeffmonteiro61@icloud.com>
2021-09-03 13:32:57 -0300

    NewVersion

--- App.js
+++ App.js
@@ -1,55 +1,72 @@
-import React from 'react';
-import { Alert, Dimensions, Image, Pressable, StyleSheet, View } from 'react-native';
+import React from 'react'
+import { Alert, Dimensions, Image, Pressable, StyleSheet, View } from 'react-native'
 
-import papel from 'src./img/papel.png'
-import pedra from 'src./img/pedra.png'
-import tesoura from 'src./img/tesoura.png'
+import pedra from './src/img/pedra.jpg'
+import papel from './src/img/papel.jpg'
+import tesoura from './src/img/tesoura.jpg'
 
-const {height} = Dimension.get('window')
-const imgHeader = {url: 'https://images.app.goo.gl/XpEi6tPArQA7H9RWA.png'}
+const { height } = Dimensions.get('window')
+
+const imgHeader = {url: 'https://mega.ibxk.com.br/2014/05/12/12142817719455.png', width: 300, height: 300}
 
 function App(props) {
   function handleButton(button){
-    const jogadas = ['Pedra','Papel','Tesoura']
+    const jogadas = ['pedra','papel','tesoura']
     const indice = Math.floor(Math.random() * jogadas.length)
     const comp = jogadas[indice]
     let resultado = ''
 
-    Alert.alert = {'Atenção!', `A sua jogada foi ${button} e a do computador foi ${comp} e o resultado foi ${resultado}`}
+    if (button === comp) resultado = 'Vocês Empataram!'
+		else if (
+			(button === 'pedra' && comp !== 'papel') ||
+			(button === 'papel' && comp !== 'tesoura') ||
+			(button === 'tesoura' && comp !== 'pedra')
+		)
+			resultado = 'Você venceu!'
+		else resultado = 'O App venceu!'
+
+    Alert.alert('Atenção!', `A sua jogada foi ${button} e a do computador foi ${comp}! ${resultado}`)
   }
   
   return (
-    <View style={styles.container}>
-      <View style={styles.panel}>
-        <Image source={imgHeader} />
+      <View style={styles.container}>
+        <View style={styles.panel}>
+          <Image source={imgHeader} />
+        </View>
+        <View style={styles.console}>
+          <Pressable onPress={handleButton.bind(this,'pedra')}>
+            <Image style={styles.img} source={pedra} />
+          </Pressable>
+          <Pressable onPress={handleButton.bind(this,'papel')}>
+            <Image style={styles.img} source={papel} />
+          </Pressable>
+          <Pressable onPress={handleButton.bind(this,'tesoura')}>
+            <Image style={styles.img} source={tesoura}/>
+          </Pressable>
+        </View>
       </View>
-      <View style={styles.console}>
-        <Pressable>
-
-        </Pressable>
-        <Pressable>
-
-        </Pressable>
-        <Pressable>
-          
-        </Pressable>
-      </View>
-    </View>
-  )
-}
+    )
+  }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#00ff78',
+		height: height,
+		alignItems: 'center',
+		justifyContent: 'space-evenly',
   },
-  panel:{
-
-  }
+  img:{
+    height: 100,
+		width: 60,
+		margin: 28,
+    borderRadius: 8,
+  },
   console:{
-
-  }
+    flexDirection: 'row',
+  },
+  panel: {
+    backgroundColor: '#f3f3f3',
+    borderRadius: 10,
+  },
 })
-export default App()
+export default App
