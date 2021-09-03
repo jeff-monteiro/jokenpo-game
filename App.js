26b72e1cbf758067ce265055e75d37736eaf6972
Jefferson <jeffmonteiro61@icloud.com>
2021-09-03 12:07:54 -0300

    buttons

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import papel from 'src./img/papel.png'
 import pedra from 'src./img/pedra.png'
 import tesoura from 'src./img/tesoura.png'
 
-function {height} = Dimension.get('window')
+const {height} = Dimension.get('window')
+const imgHeader = {url: 'https://images.app.goo.gl/XpEi6tPArQA7H9RWA.png'}
 
 function App(props) {
   function handleButton(button){
@@ -13,15 +14,25 @@ function App(props) {
     const indice = Math.floor(Math.random() * jogadas.length)
     const comp = jogadas[indice]
     let resultado = ''
+
+    Alert.alert = {'Atenção!', `A sua jogada foi ${button} e a do computador foi ${comp} e o resultado foi ${resultado}`}
   }
   
   return (
     <View style={styles.container}>
       <View style={styles.panel}>
-      
+        <Image source={imgHeader} />
       </View>
       <View style={styles.console}>
-      
+        <Pressable>
+
+        </Pressable>
+        <Pressable>
+
+        </Pressable>
+        <Pressable>
+          
+        </Pressable>
       </View>
     </View>
   )
@@ -38,7 +49,7 @@ const styles = StyleSheet.create({
 
   }
   console:{
-    
+
   }
 })
 export default App()
